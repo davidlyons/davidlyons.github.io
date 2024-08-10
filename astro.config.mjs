8b6051f9c2b003371dfdb91c0b6a65884cd904a5
David Lyons <david.scott.lyons@gmail.com>
2024-08-10 15:13:29 -0700

    removed sass, added tailwind base layer

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,24 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidlyons.dev',
-  integrations: [mdx(), sitemap(), icon(), tailwind()],
+  integrations: [mdx(), sitemap(), icon(), tailwind({ applyBaseStyles: false })],
   compressHTML: false,
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro', // https://shiki.style/themes
+      // remove trailing empty line from <pre> code blocks
+      // https://github.com/shikijs/shiki/issues/3#issuecomment-2272168959
+      transformers: [
+        {
+          preprocess(code) {
+            if (code.endsWith('\n')) {
+              code = code.slice(0, -1);
+            }
+
+            return code;
+          },
+        },
+      ],
     },
   },
 });
